323e8ac09038aa2095d8248868adfd74d085b345
Oskar Kalitzki <oskar@preferens.se>
2021-10-20 23:56:23 +0200

    Reorderable ListView (spelare)

---
+++ lib/models/spelare.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class User {
+  String name;
+
+  User({required this.name});
+}
--- lib/screens/starta_runda.dart
+++ lib/screens/starta_runda.dart
@@ -1,5 +1,8 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_networth_app/data/data.dart';
+import 'package:flutter_networth_app/models/spelare.dart';
 import 'package:flutter_networth_app/screens/speltips.dart';
 
 class StartaRunda extends StatefulWidget {
@@ -8,8 +11,13 @@ class StartaRunda extends StatefulWidget {
 }
 
 class _NetworthPageState extends State<StartaRunda> {
-  List<String> litems = [];
-  final TextEditingController eCtrl = new TextEditingController();
+  List<User> spelare = [];
+
+  void initState() {
+    super.initState();
+
+    spelare = getUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,52 +32,92 @@ class _NetworthPageState extends State<StartaRunda> {
         elevation: 0,
       ),
       body: Column(
-        children: <Widget>[
-          new TextField(
-            controller: eCtrl,
-            onSubmitted: (text) {
-              litems.add(text);
-              eCtrl.clear();
-              setState(() {});
-            },
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Text(
+              "Här kan du lägga till alla spelare i rundan. Du kan även dra-och-släppa om du vill ändra ordningen. Lycka till!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14),
+            ),
           ),
-          
-          new Expanded(
-            child: new ListView.builder(
-                itemCount: litems.length,
-                itemBuilder: (BuildContext ctxt, int Index) {
-                  return new Text(litems[Index]);
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Lägg till spelare"),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: new ReorderableListView.builder(
+                itemCount: spelare.length,
+                onReorder: (oldIndex, newIndex) => setState(() {
+                      final index =
+                          newIndex > oldIndex ? newIndex - 1 : newIndex;
+                      final user = spelare.removeAt(oldIndex);
+                      spelare.insert(index, user);
+                    }),
+                itemBuilder: (context, index) {
+                  final user = spelare[index];
+                  return buildPlayer(index, user);
                 }),
           ),
+          SizedBox(height: 20),
           ElevatedButton(
-            child: Text("Starta spel"),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Speltips()),
-              );
-            },
+            onPressed: () {},
+            child: Text("Starta"),
           ),
         ],
       ),
     );
-
-    // Column(
-    //   crossAxisAlignment: CrossAxisAlignment.center,
-    //   children: [
-    //     Text(
-    //       'Här startar man rundan',
-    //       style: Theme.of(context).textTheme.headline5,
-    //       textAlign: TextAlign.center,
-    //     ),
-    //     Card(
-    //       child: Container(
-    //         color: Colors.white,
-    //         height: 60,
-    //         width: width * 0.8,
-    //       ),
-    //     ),
-    //   ],
-    // ),
   }
+
+  Widget buildPlayer(int index, User spelare) => ListTile(
+        key: ValueKey(spelare),
+        title: Container(
+          width: 200,
+          height: 50,
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () => edit(index),
+                  child: Text(
+                    spelare.name,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () => remove(index),
+                  icon: Icon(Icons.close),
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  void remove(int index) => setState(() => spelare.removeAt(index));
+
+  void edit(int index) => showDialog(
+        context: context,
+        builder: (context) {
+          final user = spelare[index];
+
+          return AlertDialog(
+            content: TextFormField(
+              initialValue: user.name,
+              onFieldSubmitted: (_) => Navigator.of(context).pop(),
+              onChanged: (name) => setState(() => user.name = name),
+            ),
+          );
+        },
+      );
 }
